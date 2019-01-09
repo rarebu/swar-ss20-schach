0339336d51844920b80d0710a77f8aaa54e032cc
rarebu <rafthebee@gmail.com>
2019-01-09 20:04:26 +0100

    worked on controllerspec: absolutely not done yet observablespec: absolutely not done yet matrixspec: almost done pawnspec: almost done bishopspec: done kingspec: done

--- src/test/scala/de/htwg/se/Schach/util/ObservableSpec.scala
+++ src/test/scala/de/htwg/se/Schach/util/ObservableSpec.scala
@@ -1,18 +1,13 @@
 package de.htwg.se.Schach.util
 
-import de.htwg.se.Schach.model.{Bishop, Colour, Coordinates, Field}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
 
 @RunWith(classOf[JUnitRunner])
 class ObservableSpec extends WordSpec with Matchers {
-  "The Observable object" when {
-    "new" should {
-      val observable = new Observable
-      "have a Vector as subscriber" in {
-        observable.subscribers should be(Vector())
-      }
-    }
+  "An Observable" should {
+    val observable = new Observable
+
   }
 }
