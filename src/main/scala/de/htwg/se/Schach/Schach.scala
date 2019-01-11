6a2fec2066a9a10eaa96fea63e25e6c81184b335
rarebu <rafthebee@gmail.com>
2019-01-11 13:08:41 +0100

    first gui step: bringing some graphical stuff to life

--- src/main/scala/de/htwg/se/Schach/Schach.scala
+++ src/main/scala/de/htwg/se/Schach/Schach.scala
@@ -3,6 +3,7 @@ package de.htwg.se.Schach
 import de.htwg.se.Schach.controller.Controller
 import de.htwg.se.Schach.model.{Field, Player}
 import de.htwg.se.Schach.aview.TUI
+import de.htwg.se.Schach.aview.gui.SwingGui
 
 import scala.io.StdIn.readLine
 
@@ -10,6 +11,7 @@ private[Schach] object Schach {
   def main(args: Array[String]): Unit = {
     val controller = new Controller(new Field())
     val tui = new TUI(controller)
+    val gui = new SwingGui(controller)
     controller.notifyObservers()
     var input: String = ""
     do {
