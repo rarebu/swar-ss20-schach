95270f2bee4cc3c9c8b028690bc0d2f9dc3da5fb
unclec <carrylol@aol.com>
2020-06-24 03:32:07 +0200

    mongoDB - more testing for creation bug

--- src/main/scala/de/htwg/se/Schach/model/database/mongoDB/LogicDatabaseMongoDB.scala
+++ src/main/scala/de/htwg/se/Schach/model/database/mongoDB/LogicDatabaseMongoDB.scala
@@ -21,7 +21,9 @@ class LogicDatabaseMongoDB extends LogicDatabaseInterface {
       "toChange" -> {if(fieldDatabase.toChange.isDefined) fieldDatabase.toChange.get else ""}, "removedFigures" -> fieldDatabase.removedFigures,
       "roundCount" -> fieldDatabase.roundCount)
 
-    Try(if(Await.result(collection.countDocuments().toFuture(), Duration.Inf) > 0)
+    val g = Await.result(collection.countDocuments().toFuture(), Duration.Inf)
+    println("size is: " + g)
+    Try(if(g > 0)
       {
         println("create")
         Await.result(collection.insertOne(document).toFuture(), Duration.Inf)
